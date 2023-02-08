b2ef676f001cdf7400d9d824c71cde3efcf5304e
Diana Lauzhyna <dlauzhyna@gmail.com>
2023-02-08 23:44:36 +0200

    cors to origin

--- src/services/CurrencyApi.js
+++ src/services/CurrencyApi.js
@@ -1,6 +1,6 @@
 const fetchExchangeRate = async () => {
   const response = await fetch(
-    "https://api.privatbank.ua/p24api/pubinfo?exchange&json&coursid=5", { mode: 'no-cors'}
+    "https://api.privatbank.ua/p24api/pubinfo?exchange&json&coursid=5", { mode: "same-origin"}
   );
   if (!response.ok) {
     throw new Error(response.status);
